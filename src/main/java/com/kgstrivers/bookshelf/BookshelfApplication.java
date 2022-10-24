e2146b653216032a0fab52759f90361fc2ca6906
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2022-10-25 02:15:16 +0530

    Logger implemented using Log4j

--- src/main/java/com/kgstrivers/bookshelf/BookshelfApplication.java
+++ src/main/java/com/kgstrivers/bookshelf/BookshelfApplication.java
@@ -1,13 +1,19 @@
 package com.kgstrivers.bookshelf;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookshelfApplication {
 
+	private static Logger log = LogManager.getLogger(BookshelfApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BookshelfApplication.class, args);
+
+
+		log.info("=======================Server Started=========================");
 	}
 
 }
--- src/main/java/com/kgstrivers/bookshelf/RestControllers/APIControllers.java
+++ src/main/java/com/kgstrivers/bookshelf/RestControllers/APIControllers.java
@@ -1,14 +1,20 @@
 package com.kgstrivers.bookshelf.RestControllers;
 
 
+import com.kgstrivers.bookshelf.BookshelfApplication;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class APIControllers {
+
+    private static Logger log = LogManager.getLogger(APIControllers.class);
     @GetMapping("/")
     public String welcome()
     {
+        log.info("=======================Entered into Welcome Function=========================");
         return "Welcome";
     }
 }
